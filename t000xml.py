b20076f43e8eed6c39e00d2ee5840a38d4a28482
Reginaldo J Santos <32583136+regijs@users.noreply.github.com>
2021-09-10 19:41:58 -0300

    Update t000xml.py

--- t000xml.py
+++ t000xml.py
@@ -2,17 +2,16 @@
 # -*- coding: utf-8 -*-
 """
 
-@author: Reginaldo J. Santos
-DMAt-ICEx-UFMG
-regijs@gmail.com
+@author: regi
 
-Example:
+Foi usado assim:
 t000xml(range(1,3),range(-2,3),range(-2,3))
 
 """
 
 import math
 import sympy as sy
+from sympy import I
     
 
 def t000xml(aa,bb,cc):
@@ -26,21 +25,22 @@ def t000xml(aa,bb,cc):
 
     
     fid= open('t-0-0-0.xml','a')
-    nq=0
     x = sy.Symbol('x')
+    nq=0
     for a in aa:
         for b in bb:
             for c in cc:
                     if (a>0)and(abs(b)+abs(c)>0):
                         if (math.gcd(math.gcd(a,b),c)==1):
                             nq=nq+1
+                            Delta=b**2-4*a*c
                             fid.write('<question type="multichoice">\n')
                             fid.write('    <name>\n')
-                            fid.write('      <text>[0.0][0]a='+str(a)+',b='+str(b)+',c='+str(c)+'</text>\n')
+                            fid.write('      <text>[0.0][0]a='+str(a)+',b='+str(b)+',c='+str(c)+',Delta='+str(Delta)+'</text>\n')
                             fid.write('    </name>\n')
                             fid.write('    <questiontext format="html">\n')
                             fid.write('      <text><![CDATA[ ')
-                            fid.write('<p>Determine a(s) solução(ões) da equação do 2<sup>o</sup> grau</p>\n')
+                            fid.write('<p>Determine a(s) solução(ões) da equação</p>\n')
                             fid.write('<p>\\[  \n')
                             fid.write(' '+sy.latex(a*x**2 + b*x +c)+'=0. \n')
                             fid.write('\\]</p> \n')
@@ -48,51 +48,33 @@ def t000xml(aa,bb,cc):
             #
                             fid.write('    <generalfeedback format="html"> \n')
                             fid.write('      <text><![CDATA[ ')
-                            fid.write('<p>Para esta equação \\(a='+str(a)+'\\), \\(b='+str(b)+'\\) e \\(c='+str(c)+'\\),</p> \n')
-                            Delta=b**2-4*a*c
+                            fid.write('<p>Esta é uma equação de 2<sup>o</sup> grau da forma \\(ax^2+bx+c=0\\), com \\(a='+str(a)+'\\), \\(b='+str(b)+'\\) e \\(c='+str(c)+'\\).</p> \n')
                             fid.write('<p>\\[ \n')
                             fid.write('\\Delta=b^2-4ac='+str(Delta)+'. \n ')
                             fid.write('\\]</p> \n')
-                            if (b!=0):
-                                if (Delta>0):
-                                    fid.write('<p>Como \\(\\Delta > 0\\), esta equação tem duas raizes reais:</p>\n')
-                                    fid.write('<p>\\[ \n')
-                                    fid.write('x_{1,2}=-\\frac{b}{2a}\pm\\frac{\\sqrt{\\Delta}}{2a}='+sy.latex(sy.sympify(sy.Rational(-b,2*a)))+'\\pm '+sy.latex(sy.sympify(sy.sqrt(Delta)/(2*a)))+'.\n ')
-                                    fid.write('\\]</p> \n')
-                                    fid.write('<p>Ou seja,</p>\n')
-                                    fid.write('<p>\\[ \n')
-                                    fid.write('x_1='+sy.latex(sy.sympify(sy.Rational(-b,2*a)-sy.sqrt(Delta)/(2*a)))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(-b,2*a)+sy.sqrt(Delta)/(2*a)))+'.\n ')
-                                    fid.write('\\]</p> \n')
-                                if (Delta<0):
-                                    fid.write('<p>Como \\(\\Delta < 0\\), esta equação tem duas raizes complexas:</p>\n')
-                                    fid.write('<p>\\[ \n')
-                                    fid.write('x_{1,2}=-\\frac{b}{2a}\pm\\frac{\\sqrt{\\Delta}}{2a}\\,i='+sy.latex(sy.sympify(sy.Rational(-b,2*a)))+'\\pm '+sy.latex(sy.sympify(sy.sqrt(-Delta)/(2*a)))+'\\;i.\n ')
-                                    fid.write('\\]</p> \n')
-                                if (Delta==0):
-                                    fid.write('Como \\(\\Delta = 0\\), esta equação tem somente uma raiz real:\n')
-                                    fid.write('<p>\\[ \n')
-                                    fid.write('x=-\\frac{b}{2a}='+sy.latex(sy.sympify(sy.Rational(-b,2*a)))+'.\n ')
-                                    fid.write('\\]</p> \n')
-                            elif (b==0):
-                                if (Delta>0):
-                                    fid.write('<p>Como \\(\\Delta > 0\\), esta equação tem duas raizes reais:</p>\n')
-                                    fid.write('<p>\\[ \n')
-                                    fid.write('x_{1,2}=-\\frac{b}{2a}\pm\\frac{\\sqrt{\\Delta}}{2a}=\\pm '+sy.latex(sy.sympify(sy.sqrt(Delta)/(2*a)))+'.\n ')
-                                    fid.write('\\]</p> \n')
-                                    fid.write('<p>Ou seja,</p>\n')
-                                    fid.write('<p>\\[ \n')
-                                    fid.write('x_1='+sy.latex(sy.sympify(sy.Rational(-b,2*a)-sy.sqrt(Delta)/(2*a)))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(-b,2*a)+sy.sqrt(Delta)/(2*a)))+'.\n ')
-                                    fid.write('\\]</p> \n')
-                                if (Delta<0):
-                                    fid.write('<p>Como \\(\\Delta < 0\\), esta equação tem duas raizes complexas:</p>\n')
-                                    fid.write('<p>\\[ \n')
-                                    fid.write('x_{1,2}=-\\frac{b}{2a}\pm\\frac{\\sqrt{\\Delta}}{2a}\\,i=\\pm '+sy.latex(sy.sympify(sy.sqrt(-Delta)/(2*a)))+'\\;i.\n ')
-                                    fid.write('\\]</p> \n')
-                                if (Delta==0):
-                                    fid.write('Como \\(\\Delta = 0\\), esta equação tem somente uma raiz real:\n')
-                                    fid.write('<p>\\[ \n')
-                                    fid.write('x=-\\frac{b}{2a}='+sy.latex(sy.sympify(sy.Rational(-b,2*a)))+'.\n ')
-                                    fid.write('\\]</p> \n')
+                            if (Delta>0):
+                                fid.write('<p>Como \\(\\Delta > 0\\), esta equação tem duas raizes reais:</p>\n')
+                                fid.write('<p>\\[ \n')
+                                fid.write('x_{1,2}=\\frac{-b\\pm\\sqrt{\\Delta}}{2a}.\n ')
+                                fid.write('\\]</p> \n')
+                                fid.write('<p>Ou seja,</p>\n')
+                                fid.write('<p>\\[ \n')
+                                fid.write('x_1='+sy.latex(sy.sympify(sy.Rational(-b,2*a)-sy.sqrt(Delta)/(2*a)))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(-b,2*a)+sy.sqrt(Delta)/(2*a)))+'.\n ')
+                                fid.write('\\]</p> \n')
+                            if (Delta<0):
+                                fid.write('<p>Como \\(\\Delta < 0\\), esta equação tem duas raizes complexas:</p>\n')
+                                fid.write('<p>\\[ \n')
+                                fid.write('x_{1,2}=\\frac{-b\\pm\\sqrt{-\\Delta}\\, i}{2a}.\n ')
+                                fid.write('\\]</p> \n')
+                                fid.write('<p>Ou seja,</p>\n')
+                                fid.write('<p>\\[ \n')
+                                fid.write('x_1='+sy.latex(sy.sympify(sy.Rational(-b,2*a)-sy.sqrt(-Delta)/(2*a)*I))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(-b,2*a)+sy.sqrt(-Delta)/(2*a)*I))+'.\n ')
+                                fid.write('\\]</p> \n')
+                            if (Delta==0):
+                                fid.write('Como \\(\\Delta = 0\\), esta equação tem somente uma raiz real:\n')
+                                fid.write('<p>\\[ \n')
+                                fid.write('x=-\\frac{b}{2a}='+sy.latex(sy.sympify(sy.Rational(-b,2*a)))+'.\n ')
+                                fid.write('\\]</p> \n')
                             fid.write(']]></text> \n ')
                             fid.write('    </generalfeedback> \n')
                 #            #
@@ -102,47 +84,54 @@ def t000xml(aa,bb,cc):
                             fid.write(' <text>Sua resposta está parcialmente correta.</text>\n </partiallycorrectfeedback>\n <incorrectfeedback format="html">\n ')
                             fid.write(' <text>Sua resposta está incorreta.</text>\n </incorrectfeedback>\n <shownumcorrect/>\n ')
                 #            #
-                            if (Delta>=0):#(a) or (c)
-                                fid.write('    <answer fraction="100" format="html"> \n')#a
-                            else:
-                                fid.write('    <answer fraction="0" format="html"> \n')#a
+                            fid.write('    <answer fraction="100" format="html"> \n')#a
                             fid.write('      <text><![CDATA[ ')
                             if (Delta>0):
                                 fid.write('<p>\\(x_1='+sy.latex(sy.sympify(sy.Rational(-b,2*a)-sy.sqrt(Delta)/(2*a)))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(-b,2*a)+sy.sqrt(Delta)/(2*a)))+' \\)</p>\n')
                             elif(Delta<0):
-                                if (b!=0):
-                                    fid.write('<p>\\(x_{1,2}='+sy.latex(sy.sympify(sy.Rational(-b,2*a)))+'\\pm '+sy.latex(sy.sympify(sy.sqrt(abs(Delta))/(2*a)))+'\\) </p>')#a
-                                elif (b==0):
-                                    fid.write('<p>\\(x_{1,2}=\\pm '+sy.latex(sy.sympify(sy.sqrt(abs(Delta))/(2*a)))+'\\) </p>')#a
-                            elif (abs(Delta)==0):
-                                fid.write('<p>\\(x='+sy.latex(sy.sympify(sy.Rational(-b,2*a)))+'\\) </p>')#a
+                                fid.write('<p>x\\(_1='+sy.latex(sy.sympify(sy.Rational(-b,2*a)-sy.sqrt(-Delta)/(2*a)*I))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(-b,2*a)+sy.sqrt(-Delta)/(2*a)*I))+'\\) </p>\n')
+                            elif (Delta==0):
+                                fid.write('<p>\\(x='+sy.latex(sy.sympify(sy.Rational(-b,2*a)))+'\\) </p>\n')
                             fid.write(']]></text> \n ')
                             fid.write('      <feedback format="html">\n <text></text>\n </feedback>\n </answer>\n ')
-                            if (Delta<0):# (b)
-                                fid.write('    <answer fraction="100" format="html"> \n')#b
-                            else:
-                                fid.write('    <answer fraction="0" format="html"> \n')#b
+                            fid.write('    <answer fraction="0" format="html"> \n')#b
                             fid.write('      <text><![CDATA[ ')
-                            if (abs(Delta)>0):
-                                if (b==0):
-                                    fid.write('<p>\\(x_{1,2}=\\pm '+sy.latex(sy.sympify(sy.sqrt(abs(Delta))/(2*a)))+' \\;i\\) </p>')#a
-                                elif(b!=0):
-                                    fid.write('<p>\\(x_{1,2}='+sy.latex(sy.sympify(sy.Rational(-b,2*a)))+'\\pm '+sy.latex(sy.sympify(sy.sqrt(abs(Delta))/(2*a)))+' \\;i\\) </p>')#a
-                            if (abs(Delta)==0):
-                                fid.write('<p>\\(x='+sy.latex(sy.sympify(sy.Rational(-b,a)))+'\\) </p>')#a
+                            if (Delta>0):
+                                fid.write('<p>\\(x_1='+sy.latex(sy.sympify(sy.Rational(b,2*a)-sy.sqrt(Delta)/(2*a)))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(b,2*a)+sy.sqrt(Delta)/(2*a)))+' \\)</p>\n')
+                            elif(Delta<0):
+                                fid.write('<p>\\(x_1='+sy.latex(sy.sympify(sy.Rational(b,2*a)-sy.sqrt(-Delta)/(2*a)*I))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(b,2*a)+sy.sqrt(-Delta)/(2*a)*I))+'\\) </p>\n')
+                            elif (Delta==0):
+                                fid.write('<p>\\(x='+sy.latex(sy.sympify(sy.Rational(b,2*a)))+'\\) </p>')
                             fid.write(']]></text> \n ')
                             fid.write('      <feedback format="html">\n <text></text>\n </feedback>\n </answer>\n ')
                             fid.write('    <answer fraction="0" format="html"> \n')#c
                             fid.write('      <text><![CDATA[ ')
-                            if (Delta<0):
-                                if (b!=0):
-                                    fid.write('<p>\\(x_{1,2}='+sy.latex(sy.sympify(sy.Rational(-b,a)))+'\\pm '+sy.latex(sy.sympify(sy.sqrt(abs(Delta))/(a)))+' \\;i\\) </p>')#a
-                                elif(b==0):
-                                    fid.write('<p>\\(x_{1,2}=\\pm '+sy.latex(sy.sympify(sy.sqrt(abs(Delta))/(a)))+' \\;i\\) </p>')#a
-                            elif(Delta>0):
-                                fid.write('<p>\\(x_1='+sy.latex(sy.sympify(sy.Rational(-b,2*a)-sy.sqrt(Delta)/(a)))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(-b,2*a)+sy.sqrt(Delta)/(a)))+' \\)</p>\n')
-                            if (abs(Delta)==0):
-                                fid.write('<p>\\(x='+sy.latex(sy.sympify(sy.Rational(-2*b,a)))+'\\) </p>')#a
+                            if (Delta>0):
+                                fid.write('<p>\\(x_1='+sy.latex(sy.sympify(sy.Rational(-b,a)-sy.sqrt(Delta)/(a)))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(-b,a)+sy.sqrt(Delta)/(a)))+' \\)</p>\n')
+                            elif(Delta<0):
+                                fid.write('<p>\\(x_1='+sy.latex(sy.sympify(sy.Rational(-b,a)-sy.sqrt(-Delta)/(a)*I))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(-b,a)+sy.sqrt(-Delta)/(a)*I))+'\\) </p>\n')
+                            elif (Delta==0):
+                                fid.write('<p>\\(x='+sy.latex(sy.sympify(sy.Rational(-b,a)))+'\\) </p>\n')
+                            fid.write(']]></text> \n ')
+                            fid.write('      <feedback format="html">\n <text></text>\n </feedback>\n </answer>\n ')
+                            fid.write('    <answer fraction="0" format="html"> \n')#d
+                            fid.write('      <text><![CDATA[ ')
+                            if (Delta>0):
+                                fid.write('<p>\\(x_1='+sy.latex(sy.sympify(sy.Rational(-b,2*a)-sy.sqrt(Delta)/(2*a)*I))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(-b,2*a)+sy.sqrt(Delta)/(2*a)*I))+' \\)</p>\n')
+                            elif(Delta<0):
+                                fid.write('<p>\\(x_1='+sy.latex(sy.sympify(sy.Rational(-b,2*a)-sy.sqrt(-Delta)/(2*a)))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(-b,2*a)+sy.sqrt(-Delta)/(2*a)))+'\\) </p>\n')
+                            elif (Delta==0):
+                                fid.write('<p>\\(x_1='+sy.latex(sy.sympify(sy.Rational(-b,2*a)))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(b,2*a)))+'\\) </p>')
+                            fid.write(']]></text> \n ')
+                            fid.write('      <feedback format="html">\n <text></text>\n </feedback>\n </answer>\n ')
+                            fid.write('    <answer fraction="0" format="html"> \n')#e
+                            fid.write('      <text><![CDATA[ ')
+                            if (Delta>0):
+                                fid.write('<p>\\(x='+sy.latex(sy.sympify(sy.Rational(-b,2*a)-sy.sqrt(Delta)/(2*a)))+'\\)</p>\n')
+                            elif(Delta<0):
+                                fid.write('<p>\\(x='+sy.latex(sy.sympify(sy.Rational(-b,2*a)-sy.sqrt(-Delta)/(2*a)))+'\\) </p>\n')
+                            elif (Delta==0):
+                                fid.write('<p>\\(x_1='+sy.latex(sy.sympify(sy.Rational(-b,a)))+'\\text{ e }x_2='+sy.latex(sy.sympify(sy.Rational(b,a)))+'\\) </p>')
                             fid.write(']]></text> \n ')
                             fid.write('      <feedback format="html">\n <text></text>\n </feedback>\n </answer>\n ')
                             fid.write('  </question>\n\n')
